6bfb7b1139948e7591d3274569250e8aa1594bab
ajkumarrayoff@gmail.com <ajkumarrayoff@gmail.com>
2021-02-11 03:41:08 +0530

    codeforces problem 1475A

---
+++ Codeforces/Odd Divisor 1475A.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()        //Codeforces problem 1475A "Odd Divisor"
+{
+    int i,n,j,f;
+    int long m;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%ld",&m);
+        f=0;j=3;
+        if(m==2)
+            f=0;
+        while(j<=m)
+        {
+            if(m%j==0)
+                f=1;
+            j=j+2;
+        }
+        if(f==1)
+            printf("yes\n");
+        else
+            printf("no\n");
+    }
+    return 0;
+}
