21f05bb8c033da76619f3a11d036e7217d759b93
ajkumarrayoff@gmail.com <ajkumarrayoff@gmail.com>
2021-02-20 23:19:36 +0530

    added binary search

--- assignment/DSA/binary_search.c
+++ assignment/DSA/binary_search.c
@@ -1,8 +1,14 @@
 //binary search
 /*
- binary search is a fast search algorithm with run-time complexity of O(log n). This search algorithm works on the principle of divide and conquer. For this algo to work properly,the data collection should be in the sorted form.
+ binary search is a fast search algorithm with run-time complexity of O(log n).
+This search algorithm works on the principle of divide and conquer.
+ For this algo to work properly,the data collection should be in the sorted form.
 
-Binary search looks for a particular item by comparing the middle most item of the collection. If a match occurs, then the index of item is returned. If the middle item is greater than the item, then the item is searched in the sub-array to the left of the middle item. Otherwise, the item is searched for in the sub-array to the right of the middle item. This process continues on the sub-array as well untill the size of the sub-array reduces to zero.
+Binary search looks for a particular item by comparing the middle most item of the collection.
+ If a match occurs, then the index of item is returned.
+ If the middle item is greater than the item, then the item is searched in the sub-array to the left of the middle item.
+ Otherwise, the item is searched for in the sub-array to the right of the middle item.
+ This process continues on the sub-array as well untill the size of the sub-array reduces to zero.
  */
 
 #include <stdio.h>
