ebacc573d70dded30c4f1d5149bf66eb5213ea6c
ajkumarrayoff@gmail.com <ajkumarrayoff@gmail.com>
2021-09-07 19:30:35 +0530

    Sept 7

---
+++ leetcode/reverseList.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    ListNode *reverseList(ListNode *head)
+    {
+        ListNode *prev = NULL;
+        ListNode *next = NULL;
+        while (head != NULL)
+        {
+            next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;
+        }
+        head = prev;
+        return head;
+    }
+};
