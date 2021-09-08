be64016b763b2311fc90580cd31ff499cac8ebe0
ajkumarrayoff@gmail.com <ajkumarrayoff@gmail.com>
2021-09-08 23:32:10 +0530

    Sept 8

---
+++ leetcode/shiftingLetters.cpp
@@ -0,0 +1,14 @@
+class Solution
+{
+public:
+    string shiftingLetters(string s, vector<int> &shifts)
+    {
+        int raySize = shifts.size();
+        for (auto i = shifts.end(); i != shifts.begin(); i--)
+        {
+            while (raySize--)
+            {
+            }
+        }
+    }
+};
