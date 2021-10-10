86f8d65fe3b0c622d1375b93f49293d984aeeee0
ajkumarrayoff@gmail.com <ajkumarrayoff@gmail.com>
2021-10-10 17:10:24 +0530

    Oct 10

---
+++ leetcode/maxDepth.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int maxDepth(TreeNode *root)
+    {
+        if (root == NULL)
+            return 0;
+        else
+        {
+            int l = maxDepth(root->left);
+            int r = maxDepth(root->right);
+
+            if (l > r)
+                return (l + 1);
+            else
+                return (r + 1);
+        }
+    }
+};
---
+++ leetcode/rangeBitwiseAnd.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int rangeBitwiseAnd(int left, int right)
+    {
+        if ((left == 0) || (right == 0))
+            return 0;
+
+        if ((int)log2(left) != (int)log2(right))
+            return 0;
+
+        int ans = left;
+        for (long i = left; i <= right; i++)
+            ans &= i;
+
+        return ans;
+    }
+};
