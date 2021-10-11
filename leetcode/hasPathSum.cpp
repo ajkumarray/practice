d0d8a7e8096672156e5aa0014170740f7120a7c2
ajkumarrayoff@gmail.com <ajkumarrayoff@gmail.com>
2021-10-11 17:42:37 +0530

    Oct 11

---
+++ leetcode/hasPathSum.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool hasPathSum(TreeNode *root, int targetSum)
+    {
+        if (!root)
+            return false;
+        if (!root->left && !root->right)
+            return targetSum == root->val;
+        return hasPathSum(root->left, targetSum - root->val) || hasPathSum(root->right, targetSum - root->val);
+    }
+};
---
+++ leetcode/invertTree.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    TreeNode *invertTree(TreeNode *root)
+    {
+        if (!root)
+            return root;
+
+        invertTree(root->left);
+        invertTree(root->right);
+        swap(root->left, root->right);
+
+        return root;
+    }
+};
