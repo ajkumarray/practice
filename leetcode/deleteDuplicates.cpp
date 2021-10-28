e204200df5a7d79ac8d8d92197cfd475055d0daf
ajkumarrayoff@gmail.com <ajkumarrayoff@gmail.com>
2021-10-29 00:06:24 +0530

    Oct 29

--- leetcode/deleteDuplicates.cpp
+++ leetcode/deleteDuplicates.cpp
@@ -5,6 +5,7 @@ using namespace std;
 class Solution
 {
 public:
+    //      sorted list I
     ListNode *deleteDuplicates(ListNode *head)
     {
         if (head == NULL)
@@ -24,6 +25,35 @@ public:
                 }
                 else
                     head->next = head->next->next;
+                head = deleteDuplicates(head);
+            }
+            else
+                head->next = deleteDuplicates(head->next);
+        }
+
+        return head;
+    }
+
+    //      sorted list II
+    ListNode *deleteDuplicates(ListNode *head)
+    {
+        if (head == NULL)
+            return head;
+
+        if (head->next == NULL)
+        {
+            head->next = deleteDuplicates(head->next);
+        }
+        else
+        {
+            if (head->val == head->next->val)
+            {
+                if (head->next->next == NULL)
+                {
+                    head = NULL;
+                }
+                else
+                    head = head->next->next;
                 head = deleteDuplicates(head);
             }
             else
